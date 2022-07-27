01531d3aa9057b307b66ea506c32ec4c79e575b1
Victor <vggaia10@gmail.com>
2022-07-26 22:12:58 -0300

    add repository page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import Repository from '../pages/Repository'
 
 function MyRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/repository" element={<Repository />} />
       </Routes>
     </BrowserRouter>
   )
