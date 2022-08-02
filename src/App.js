a6e04c804a93dcf632cc3d164876f34c66b17b9f
Victor <vggaia10@gmail.com>
2022-08-02 10:27:14 -0300

    add estilizacao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import MyRoutes from './routes'
 import Routes from './routes'
+import Global from './styles/global'
+import global from './styles/global'
 
 function App() {
-  return <MyRoutes />
+  return (
+    <>
+      <MyRoutes />
+      <Global />
+    </>
+  )
 }
 
 export default App
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { Container, Form, SubmitButton, Title } from './styled'
+import { FaGithub, FaPlug } from 'react-icons/fa'
 
 // import { Container } from './styles';
 
 function Home() {
   return (
-    <div>
-      <button>aa</button>
-    </div>
+    <Container>
+      <h1>
+        <FaGithub />
+        Repositorios
+      </h1>
+      <Form>
+        <input
+          onChange={() => {}}
+          type="text"
+          placeholder="Digite um repositorio"
+        />
+
+        <SubmitButton>
+          <FaPlug color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   )
 }
 
